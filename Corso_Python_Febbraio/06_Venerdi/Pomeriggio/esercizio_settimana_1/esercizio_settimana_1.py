a97389ee385a4b2b8474a7a60472e4aaa635e95c
VeronicaVeneroso <venerosov@gmail.com>
2026-02-06 17:20:58 +0100

    Add files via upload

---
+++ Corso_Python_Febbraio/06_Venerdi/Pomeriggio/esercizio_settimana_1/esercizio_settimana_1.py
@@ -0,0 +1,15 @@
+tuple = ("Nome", "Cognome", "Età", "Indirizzo e-mail")
+nomi = []
+cognomi = []
+eta = []
+email = []
+insieme_eta = {}
+
+risposta = "si"
+
+while risposta == "si":
+    nome = input("Inserisci nome utente: ")
+    cognome = input("Inserisci cognome utente: ")
+    anni = input("Inserisci età utente: ")
+    email = input("Inserisci indirizzo e-mail utente: ")
+
