e8723e02db35f141ff85966b4a15dfeec6e84d2b
VeronicaVeneroso <venerosov@gmail.com>
2026-02-07 17:32:52 +0100

    Add files via upload

--- Corso_Python_Febbraio/06_Venerdi/Pomeriggio/esercizio_settimana_1/esercizio_settimana_1.py
+++ Corso_Python_Febbraio/06_Venerdi/Pomeriggio/esercizio_settimana_1/esercizio_settimana_1.py
@@ -1,15 +1,55 @@
-tuple = ("Nome", "Cognome", "Età", "Indirizzo e-mail")
-nomi = []
-cognomi = []
+
+utenti = []
 eta = []
-email = []
-insieme_eta = {}
+codici = []
+risposta = True
+while risposta == True:
+    scelta = input("Vuoi inserire un nuovo utente? (si/no) ").lower()
+    match scelta:
+        case "si":
+            pass
+        case "no":
+            break
+        case _:
+            print("Risposta non valida.")
+            continue
 
-risposta = "si"
+    cf = str()
+    while not(len(cf) == 16):
+        cf = input("Inserisci codice fiscale:\n")
+        if len(cf) != 16:
+            print("Il codice fiscale inserito non è corretto. Deve avere 16 caratteri. Riprova:")
 
-while risposta == "si":
-    nome = input("Inserisci nome utente: ")
-    cognome = input("Inserisci cognome utente: ")
-    anni = input("Inserisci età utente: ")
-    email = input("Inserisci indirizzo e-mail utente: ")
+    if cf in codici:
+        print("Utente già registrato.")
+    else:
+        codici.append(cf)
+        print("Inserisci età:")
+        anni = int(input())
+        if anni < 0 or anni > 120:
+            print("Età non valida.")
+            continue
+        nome = input("Inserisci nome:\n")
+        cognome = input("Inserisci cognome:\n")
+        eta.append(anni)
+        utente = (cognome, nome, anni, cf)
+        utenti.append(utente)
+        print("Nuovo utente inserito correttamente.")
 
+numero_utenti = len(utenti)
+if numero_utenti > 0:
+    print("Ci sono ", numero_utenti, "utenti. La lista completa è:")
+
+    for i in range(numero_utenti):
+        print(i+1,  utenti[i])
+
+    insieme_eta = set(eta)
+    print("Le età degli utenti sono: ", insieme_eta)
+
+    somma = 0
+    for num in eta:
+        somma = somma + num
+    media = somma / numero_utenti
+    print("L'età media degli utenti è", media)
+else:
+    print("Non ci sono utenti.")
