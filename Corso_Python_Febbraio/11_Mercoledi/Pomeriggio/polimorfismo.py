6886d6f31608f7defcf8e6728b82c35b59b9f3a7
VeronicaVeneroso <venerosov@gmail.com>
2026-02-11 17:42:54 +0100

    Create polimorfismo.py

---
+++ Corso_Python_Febbraio/11_Mercoledi/Pomeriggio/polimorfismo.py
@@ -0,0 +1,13 @@
+class Animale:
+    def emetti_suono(self):
+        print("Questo animale fa un suono")
+    
+
+class Cane(Animale):
+    def emetti_suono(self):
+        print("Bau")
+
+
+class Gatto(Animale):
+    def emetti_suono(self):
+        print("Miao")
