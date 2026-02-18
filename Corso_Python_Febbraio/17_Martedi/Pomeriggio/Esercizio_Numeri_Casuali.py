3788ba9e47c274b12dbdca8f7672e845f124af9e
VeronicaVeneroso <venerosov@gmail.com>
2026-02-18 14:04:44 +0100

    18 Febbraio

--- Corso_Python_Febbraio/17_Martedi/Pomeriggio/Esercizio_Numeri_Casuali.py
+++ Corso_Python_Febbraio/17_Martedi/Pomeriggio/Esercizio_Numeri_Casuali.py
@@ -1,3 +1,56 @@
 '''Scrivete un programma che genera 5 numeri
 casuali e li salva su un file, l’utente dovrà cercare di indovinarne almeno 2 oppure
 avrà perso.'''
+
+from random import sample
+
+def check (numero) :
+    try : 
+        int (numero)
+        return True 
+    except ValueError :
+        print ("ERRORE devi inserire un numero") 
+        return False
+
+numeri = sample(range(1, 101), 5)
+#print(numeri)
+
+with open("file_numeri.txt","w") as file: 
+    file.write ((",").join(map(str, numeri)))
+    #file.write (str(numeri).strip("[]"))
+
+with open("file_numeri.txt","r") as file: 
+    contenuto = file.read ()
+
+contenuto = contenuto.split (",") 
+#print (contenuto)
+
+game_over = False
+
+for i in range (5) :
+    while True : 
+
+        n1 = input ("inserisci il primo numero ")
+        if not check (n1) :
+            continue
+        else : 
+
+            n2 = input ("inserisci il secondo numero ")
+            if not check (n2) :
+                continue
+
+        if n1 == n2 :
+            print ("ERRORE due numeri non devono essere uguali")
+            continue
+        break 
+
+    if n1 in contenuto and n2 in contenuto :
+        print ("hai vinto ! ") 
+        game_over = True
+        break
+    elif n1 in contenuto or n2 in contenuto :
+        print ("solo un numero è corretto")
+    else :
+        print ("Entrambi i numeri sono sbagliati ")
+if not game_over :
+    print ("Hai perso e hai finito i tentativi ! ")
---
+++ Corso_Python_Febbraio/18_Mercoledi/Mattina/Gestionale_Alunni.py
@@ -0,0 +1,8 @@
+'''Trasformate il programma che abbiamo creato in
+precedenza per la gestione dei voti degli alunni in un
+programma per la gestione di una classe che utilizza un
+file come database:
+All’avvio il programma chiede se si vuole leggere l’elenco
+degli alunni e i loro voti e medie, se si vuole aggiungere un
+alunno o un voto'''
+
